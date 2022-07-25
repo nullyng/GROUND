6560474f0426dbb4a1b3f1e43bbeb69ba8ab1502
InhuJo <inhudev@gmail.com>
2022-07-25 16:49:23 +0900

    [S07P12D103-15] feat : 아이디, 비밀번호 입력 경고 문구 추가
    아이디나 비밀번호 입력하지 않고 로그인 버튼을 누르면 경고 문구 생성

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,18 +1,62 @@
 import "styles/Login/LoginPage.scss";
+import logo from "assets/images/text_logo.png";
+
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import logo from "assets/images/text_logo.png";
+
 import { useState } from "react";
 
 function LoginPage() {
   const [userId, setUserId] = useState("");
   const [userPW, setUserPW] = useState("");
+  const [idProps, setIdProps] = useState({
+    className: "login-form__field",
+    label: "아이디",
+    variant: "outlined",
+    size: "small",
+    error: false,
+    helperText: "",
+  });
+  const [pwProps, setPwProps] = useState({
+    className: "login-form__field",
+    label: "비밀번호",
+    variant: "outlined",
+    size: "small",
+  })
 
   function submitLogin() {
-    console.log(userId);
-    console.log(userPW);
+    let isLoginOk = true;
+    let newIdProps = {...idProps};
+    let newPwProps = {...pwProps};
+
+    if(userId.trim() === "") {
+      isLoginOk = false;
+      newIdProps.error = true;
+      newIdProps.helperText = "아이디를 입력해주세요";
+    }
+    else {
+      newIdProps.error = false;
+      newIdProps.helperText = "";
+    }
+
+    if(userPW.trim() === "") {
+      isLoginOk = false;
+      newPwProps.error = true;
+      newPwProps.helperText = "비밀번호를 입력해주세요";
+    }
+    else {
+      newPwProps.error = false;
+      newPwProps.helperText = "";
+    }
+
+    setIdProps(newIdProps);
+    setPwProps(newPwProps);
+
+    if(isLoginOk) {
+      // 로그인 요청
+    }
   }
 
   return (
@@ -27,21 +71,37 @@ function LoginPage() {
         <Grid className="login-form__logo" item>
           <img className="logo" src={logo} alt="text_logo" width="300px" />
         </Grid>
-        <TextField
+        {/* <TextField
           className="login-form__field"
           label="아이디"
           variant="outlined"
           size="small"
           value={userId}
+          helperText="아이디를 입력해주세요"
+          onChange={(e) => {
+            setUserId(e.target.value);
+          }}
+        /> */}
+        <TextField
+          {...idProps}
+          value={userId}
           onChange={(e) => {
             setUserId(e.target.value);
           }}
         />
-        <TextField
+        {/* <TextField
           className="login-form__field"
           label="비밀번호"
           variant="outlined"
           size="small"
+          value={userPW}
+          helperText="비밀번호를 입력해주세요"
+          onChange={(e) => {
+            setUserPW(e.target.value);
+          }}
+        /> */}
+        <TextField
+          {...pwProps}
           value={userPW}
           onChange={(e) => {
             setUserPW(e.target.value);
