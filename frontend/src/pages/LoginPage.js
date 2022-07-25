bf732486b90771d5e16628050cd0b6990f0b20ef
InhuJo <inhudev@gmail.com>
2022-07-25 15:24:50 +0900

    [S07P12D103-15] feat : 로그인 폼 생성

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,59 @@
+import "styles/Login/LoginPage.scss";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import logo from "assets/images/text_logo.png";
+
+function LoginPage() {
+  return (
+    <Container className="login-form" maxWidth="xs" fixed>
+      <Grid
+        className="login-form__wrapper"
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid className="login-form__logo" item>
+          <img className="logo" src={logo} alt="text_logo" width="300px" />
+        </Grid>
+        <TextField
+          className="login-form__field"
+          label="아이디"
+          variant="outlined"
+          size="small"
+        />
+        <TextField
+          className="login-form__field"
+          label="비밀번호"
+          variant="outlined"
+          size="small"
+        />
+        <Button className="login-form__button" variant="contained">
+          로그인
+        </Button>
+        <Button className="login-form__button" variant="contained">
+          구글 로그인
+        </Button>
+        <Button className="login-form__button" variant="contained">
+          카카오 로그인
+        </Button>
+        <Grid
+          className="login-form__bottom"
+          container
+          justifyContent="space-between"
+        >
+          <Grid item>
+            <a href="/">회원가입</a>
+          </Grid>
+          <Grid item>
+            <a href="/">아이디 찾기 / 비밀번호 찾기</a>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default LoginPage;
