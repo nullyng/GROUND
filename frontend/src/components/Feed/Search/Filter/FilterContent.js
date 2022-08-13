f64df760252c6ee9c23aef4f7b397fb2ec575f9f
Inhu Jo <inhudev@gmail.com>
2022-08-13 20:12:09 +0900

    [S07P12D103-193] refactor : FilterTitle을 FilterModal에서 FilterContent로 이동

--- frontend/src/components/Feed/Search/Filter/FilterContent.js
+++ frontend/src/components/Feed/Search/Filter/FilterContent.js
@@ -1,10 +1,22 @@
 import { useFilterState } from "./FilterContext";
 
+import FilterTitle from "./FilterTitle";
 import DateFilterItem from "./Date/DateFilterItem";
 import FilterItem from "./Others/FilterItem";
 import FilterSelect from "./Others/FilterSelect";
 
-function FilterContent({ items }) {
+const titles = [
+  { id: 0, title: "필터" },
+  { id: 1, title: "기간" },
+  { id: 2, title: "운동종목" },
+  { id: 3, title: "성별" },
+  { id: 4, title: "연령대" },
+  { id: 5, title: "지역" },
+];
+
+const items = titles.filter((title) => title.id > 1);
+
+function FilterContent() {
   const { select } = useFilterState();
 
   const filterItems = items.map((item) => (
@@ -13,6 +25,7 @@ function FilterContent({ items }) {
 
   return (
     <div className="filter-modal__inner">
+      <FilterTitle titles={titles} />
       {!select && (
         <>
           <DateFilterItem />
--- frontend/src/components/Feed/Search/Filter/FilterModal.js
+++ frontend/src/components/Feed/Search/Filter/FilterModal.js
@@ -1,22 +1,7 @@
 import "styles/Search/Filter.scss";
-
 import { Box, Modal } from "@mui/material";
-
 import { FilterProvider } from "./FilterContext";
-
-import FilterTitle from "./FilterTitle";
 import FilterContent from "./FilterContent";
-
-const titles = [
-  { id: 0, title: "필터" },
-  { id: 1, title: "기간" },
-  { id: 2, title: "운동종목" },
-  { id: 3, title: "성별" },
-  { id: 4, title: "연령대" },
-  { id: 5, title: "지역" },
-];
-
-const items = titles.filter((title) => title.id > 1);
 
 function FilterModal({ open, handleClose }) {
   const submit = (e) => {
@@ -35,9 +20,8 @@ function FilterModal({ open, handleClose }) {
       >
         <Box className={open ? "filter-modal" : "filter-modal close"}>
           <FilterProvider>
-            <FilterTitle titles={titles} />
             <form>
-              <FilterContent items={items} />
+              <FilterContent />
               <button className="filter-modal__submit" onClick={submit}>
                 설정
               </button>
